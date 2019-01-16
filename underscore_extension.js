36cb8293ec5c8348271704ce28c8512cfc24fcd2
lightwave88 <torpedo1.tw@gmail.com>
2019-01-16 14:54:11 +0800

    Add files via upload

--- underscore_extension.js
+++ underscore_extension.js
@@ -28,6 +28,7 @@
         }
 
         if (_.$$extension != null) {
+            // 避免重複
             return;
         }
         //----------------------------
@@ -117,77 +118,7 @@
                     throw error;
                 }
             });
-        }
-        //----------------------------
-        if (_.defineProperty == null) {
-            _.mixin({
-                // (Object.defineProperty)的封裝
-                defineProperty: function (obj, key, val, enumerable) {
-                    Object.defineProperty(obj, key, {
-                        value: val,
-                        enumerable: !!enumerable,
-                        writable: true,
-                        configurable: true
-                    });
-                }
-            });
-        }
-        //----------------------------
-        if (_.isPlainObject == null) {
-            _.mixin({
-                // 是否是 {}
-                // fix
-                isPlainObject: function (obj) {
-                    debugger;
-
-                    if (typeof obj != "object") {
-                        return false;
-                    }
-
-                    if (obj == null) {
-                        return false;
-                    }
-
-                    let res = Object.prototype.toString.call(obj);
-
-                    if (!/^\[object Object\]$/.test(res)) {
-                        return false;
-                    }
-
-                    if (obj.constructor !== {}.constructor) {
-                        return false;
-                    }
-
-                    return true;
-                }
-            });
-        }
-        //----------------------------
-        if (_.getClass == null) {
-            _.mixin({
-                // 比系統 typeof 能辨識更多 type
-                getClass: function (data) {
-                    let _toString = Object.prototype.toString;
-
-                    let type = typeof (data);
-
-                    if (/object/.test(type)) {
-
-                        if (data === null) {
-                            type = "null";
-                        } else {
-                            type = _toString.call(data);
-
-                            let res = /\[\w+\s+(\w+)\]/.exec(type);
-                            if (res && res[1]) {
-                                type = res[1];
-                            }
-                        }
-                    }
-                    return type;
-                },
-            });
-        }
+        }        
         //----------------------------
         // extension 的參數設定
         if (typeof _.extension !== 'undefined') {
@@ -213,30 +144,6 @@
                 }
             });
         }
-        //----------------------------
-        if (_.literals == null) {
-            // es6 模版
-            _.mixin({
-                literals: function (str) {                    
-                    str = str.replace(/^`/, '');
-                    str = str.replace(/`$/, '');
-
-                    str = str.replace(/`/g, '\\`');
-
-                    str = "`" + str + "`";
-                    //-----------------------
-
-                    let command = "for (let key in data) {\
-                        let script = 'var ' + key + '= data[\"' + key + '\"]';\
-                        eval(script)\
-                    }";
-
-                    command += 'return (' + str + ');';
-                    environment = new Function("data", command);
-
-                    return environment;
-                }
-            });
-        }
+        
     };
 })(this || {});
