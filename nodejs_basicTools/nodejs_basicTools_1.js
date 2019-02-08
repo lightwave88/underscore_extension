71610985883ea327505a5c925e8f28b8b62e22ae
lightwave88 <torpedo1.tw@gmail.com>
2019-02-08 13:09:05 +0800

    Add files via upload

--- nodejs_basicTools/nodejs_basicTools_1.js
+++ nodejs_basicTools/nodejs_basicTools_1.js
@@ -1,4 +1,4 @@
-!(function (global) {
+!(function (_global) {
 
     if (typeof (module) == 'object' && module.exports) {
         // 指定 loadash|underscode 的 path
@@ -13,13 +13,15 @@
     function factory(_) {
         if (_.readFile == null) {
             _.mixin({
-                // p1: 串接 promise
                 // path: 路徑
                 // code: 編碼
-                readFile: function (p, path, code) {
-                    if (!(p instanceof Promise)) {
-                        p1 = Promise.resolve();
-                    }
+                // 可在參數最後面串接 promise
+                readFile: function (path, code) {
+
+                    let args = Array.from(arguments);
+                    let p = args[args.length - 1];
+
+                    p = (p instanceof Promise) ? p : (Promise.resolve());
 
                     code = code || 'utf-8';
                     //------------------
@@ -29,9 +31,11 @@
                         // 讀檔
                         let r;
 
-                        data = data || {};
-                        path = (data.path != null) ? data.path : path;
-                        code = (data.code != null) ? data.code : code;
+                        data = (Array.isArray(data)) ? data : [];
+
+                        path = data[0] || path;
+                        cpde = data[1] || code;
+
 
                         return new Promise(function (res, rej) {
                             fs = require('fs');
@@ -51,16 +55,20 @@
             });
         }
         //----------------------------
-        if (_.fileExists == null) {
+        if (_.isFile == null) {
             _.mixin({
-                // p1: promise 串接
-                fileExists: function (p, path) {
-                    if (!(p instanceof Promise)) {
-                        p = Promise.resolve();
-                    }
+                // 可在參數最後面串接 promise
+                isFile: function (path) {
+                    let args = Array.from(arguments);
+                    let p = args[args.length - 1];
+
+                    p = (p instanceof Promise) ? p : (Promise.resolve());
                     //------------------
                     let fs;
                     p = p.then(function (data) {
+
+                        path = (typeof (data) == "string") ? data : path;
+
                         return new Promise(function (res, rej) {
                             debugger;
                             fs = require('fs');
@@ -70,7 +78,7 @@
                             }
                             // 找尋檔案
                             fs.stat(path, function (err, data) {
-                                debugger;
+                                // debugger;
                                 if (err) {
                                     rej(err);
                                 } else {
@@ -85,5 +93,116 @@
                 }
             });
         }
+        //----------------------------
+        if (_.pipeReadStream == null) {
+            _.mixin({
+                // 可在參數最後面串接 promise
+                pipeReadStream: function pipeReadStream(path, reader, readSize, code) {
+
+                    let def = _.deferred();
+
+                    let args = Array.from(arguments);
+                    let p = args[args.length - 1];
+
+                    p = (p instanceof Promise) ? p : (Promise.resolve());
+
+
+                    p.then(function () {
+                        let source;
+                        try {
+                            source = fs.createReadStream(path);
+
+                            if (code) {
+                                source.setEncoding(code);
+                            }
+                        } catch (error) {
+
+                            def.reject(error);
+
+                            return;
+                        }
+                        //----------------------------
+                        let buffer = [];
+                        let pause = false;
+
+                        source.on('readable', function () {
+
+                            console.log('readable....');
+
+                            let chunk;
+                            //-----------------------
+
+                            while (null !== (chunk = source.read(1024))) {
+                                console.log('得到了 %d 位元組的資料準備寫入', chunk.length);
+
+                                if (buffer.length) {
+                                    console.log('想寫入, 但 buffer 未空')
+                                    buffer.push(chunk);
+                                } else if (pause) {
+                                    console.log('被輸入勒令暫停過, 寫入 buffer')
+                                    buffer.push(chunk);
+                                } else {
+
+                                    console.log('寫入')
+                                    // 嘗試寫入
+                                    if ((reader.write(chunk)) == false) {
+                                        // 寫入已滿
+                                        console.log('寫入已滿，暫停')
+                                        source.pause();
+                                        pause = true;
+                                    }
+                                }
+                            }
+                        });
+                        //-----------------------
+                        reader.on('drain', function () {
+                            console.log('drain');
+
+                            if (!writeBuffer()) {
+                                // buffer 寫入時發生了寫入滿溢
+                                // 暫時不要喚醒 source
+                                console.log('buffer 寫入時發生了寫入滿溢');
+                                return;
+                            }
+
+                            if (source.readable && source.resume) {
+                                console.log('恢復輸出')
+                                pause = false;
+                                source.resume();
+                            }
+                        });
+                        //-----------------------
+                        source.once('error', function () {
+                            def.reject();
+                        });
+
+                        //-----------------------
+                        source.once('end', function () {
+                            def.resolve();
+                        });
+
+                        //-----------------------
+                        function writeBuffer() {
+                            let chunk;
+                            while (buffer.length) {
+                                console.log('proxy寫入');
+                                chunk = buffer[0]
+                                buffer.shift();
+
+                                if (false == reader.write(chunk)) {
+                                    console.log('proxy寫入太滿');
+                                    return false;
+                                }
+                            }
+
+                            return true;
+                        }
+                    });
+
+                    return def.promise();
+                }
+            });
+        }
+        //----------------------------
     }
 })(this || {});
