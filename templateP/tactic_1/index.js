977cf4f2229bd12751a8a44008553aa116424046
lightwave88 <torpedo1.tw@gmail.com>
2019-04-20 20:43:38 +0800

    Delete index.js

--- templateP/tactic_1/index.js
+++
@@ -1,9 +0,0 @@
-// 快速但準確較低的解析策略
-
-let FindCommandTags = require('./findCommandTags_1');
-
-module.exports = function(content){
-    let o = new FindCommandTags();
-
-    return o.solution(content);
-}
